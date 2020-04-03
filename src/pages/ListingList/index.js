9ca72a7d12a9645f957833a54fe114a70e37134e
Frederick Johnson <fritzj96@gmail.com>
2020-04-03 08:39:31 -0700

    addded: selectors & loading indicator to LL page

--- src/pages/ListingList/index.js
+++ src/pages/ListingList/index.js
@@ -1,6 +1,12 @@
 import { useSelector } from "react-redux";
-import {selectListings, addListing, loadListings } from "./listingsSlice";
+import {selectListings, selectError, selectLoading addListing, loadListings } from "./listingsSlice";
 
 export const ListingList = () => {
   const listings = useSelector(selectListings);
+  const error = useSelector(selectError);
+  const loading = useSelector(selectLoading);
+
+  if (loading) {
+    return (<h1 className="text-center">Loading...</h1>)
+  }
 }
