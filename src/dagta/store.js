e22be21769b513ba74918d29f0923d3aad217c6c
Frederick Johnson <fritzj96@gmail.com>
2020-04-03 10:57:04 -0700

    fixed: broken import

--- src/dagta/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import listingsReducer from '../data/listingsSlice';
-
-export default configureStore({
-  reducer: {
-    listings: listingsReducer
-  },
-});
